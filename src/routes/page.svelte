6206323b903ab460abf8ad6e61dd9e8382265abf
David Kamholz <lautgesetz@gmail.com>
2021-11-16 11:19:35 -0800

    initial commit

---
+++ src/routes/page.svelte
@@ -0,0 +1,13 @@
+<script context="module">
+  export async function load({ page: { query } }) {
+    return {
+      props: { param: query.get('param') }
+    };
+  }
+</script>
+
+<script>
+  export let param;
+</script>
+
+{param}
